165edf802d92b5582585394154c7b1b69483445d
Dmitriy <d.danevskyi@gmail.com>
2019-06-16 20:19:35 +0300

    Fix script

--- dimka/make_train_df.py
+++ dimka/make_train_df.py
@@ -19,14 +19,14 @@ spoof_directory = os.path.join(args.data_directory, "spoof")
 human_files = sorted(glob.iglob(
     os.path.join(human_directory, "**/*.wav"), recursive=True))
 spoof_files = sorted(glob.iglob(
-    os.path.join(human_directory, "**/*.wav"), recursive=True))
+    os.path.join(spoof_directory, "**/*.wav"), recursive=True))
 
 human_files = [os.path.join("human", os.path.basename(p)) for p in human_files]
 spoof_files = [os.path.join("spoof", os.path.basename(p)) for p in spoof_files]
 
-train_df = pd.DataFrame(
-    {"fname": human_files + spoof_files,
-    "labels": [1] * len(human_files) + [0] * len(spoof_files)}
-)
+train_df = pd.DataFrame({
+    "fname": human_files + spoof_files,
+    "labels": [1] * len(human_files) + [0] * len(spoof_files)
+})
 
 train_df.to_csv(args.output_file, index=False)
