326c71c5fef67773812cd04d57936a841ec0ae13
Dmitriy <d.danevskyi@gmail.com>
2019-06-16 19:31:13 +0300

    Add script for creating a train_df

---
+++ dimka/make_train_df.py
@@ -0,0 +1,32 @@
+import os
+import argparse
+import glob
+
+import pandas as pd
+
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("--data_directory", type=str, required=True)
+parser.add_argument("--output_file", type=str, required=True)
+
+args = parser.parse_args()
+
+
+human_directory = os.path.join(args.data_directory, "human")
+spoof_directory = os.path.join(args.data_directory, "spoof")
+
+human_files = sorted(glob.iglob(
+    os.path.join(human_directory, "**/*.wav"), recursive=True))
+spoof_files = sorted(glob.iglob(
+    os.path.join(human_directory, "**/*.wav"), recursive=True))
+
+human_files = [os.path.join("human", os.path.basename(p)) for p in human_files]
+spoof_files = [os.path.join("spoof", os.path.basename(p)) for p in spoof_files]
+
+train_df = pd.DataFrame(
+    {"fname": human_files + spoof_files,
+    "labels": [1] * len(human_files) + [0] * len(spoof_files)}
+)
+
+train_df.to_csv(args.output_file, index=False)
