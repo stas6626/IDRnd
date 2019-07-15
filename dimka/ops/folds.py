d4cfcba545486c8b27cfbcee14325f1e797259c9
Dmitriy <d.danevskyi@gmail.com>
2019-07-15 22:56:43 +0300

    Support fold grouping

--- dimka/ops/folds.py
+++ dimka/ops/folds.py
@@ -1,10 +1,15 @@
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, GroupKFold
 import numpy as np
 
 
 
-def train_validation_data_stratified(ids, labels, n_folds, seed):
+def train_validation_data_stratified(ids, labels, n_folds, seed, groups=None):
 
-    for train, valid in StratifiedKFold(
-        n_folds, shuffle=True, random_state=seed).split(ids, labels):
-        yield train, valid
+    if groups is None:
+        for train, valid in StratifiedKFold(
+            n_folds, shuffle=True, random_state=seed).split(ids, labels):
+            yield train, valid
+    else:
+        for train, valid in GroupKFold(
+            n_folds, shuffle=True, random_state=seed).split(ids, labels, groups):
+            yield train, valid
